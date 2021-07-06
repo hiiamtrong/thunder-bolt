bbfb86f33b8b0b485005c94de33d26cb7f625f02
Truong Manh Nguyen <truongmanh0912@gmail.com>
2021-07-06 19:06:21 +0700

    Handle no text in payload

--- features/tasks/tasks.controller.js
+++ features/tasks/tasks.controller.js
@@ -3,12 +3,14 @@ import { reply } from '../../helper.js'
 
 function replyWrongPattern(action, type) {
   switch (type){
-    case 'missing_require':
+    case 'help':
       return reply(action, 'Cấu trúc câu lệnh task gồm \n*name* "..." assign @Truong type 1 board "..." \n(In *đậm* là bắt buộc, type có 3 loại 1/2/3))')
     case 'missing_id':
       return reply(action, 'Type 3 yêu cầu assign')
+    case 'missing_require':
+      return reply(action, 'Thiếu name roài nhé, gõ --help hoặc -h để được hướng dẫn')
     default:
-      return reply(action, 'Unexpected')
+      return reply(action, 'Gõ --h hoặc --help để được hướng dẫn nhé')
   }
 }
 
@@ -16,13 +18,16 @@ export const mentionHandler = async (action) => {
   // handle mention of task
   // oi doi oi Young Mother lam phien toi qua hic (❁´◡`❁)
   const { text } = action.payload
+  if(!text) {
+    return replyWrongPattern(action)
+  }
   const hasName = text.match(/name "(.*?)"/gi)
   const hasAssign = text.match(/assign <(.*?)>/gi)
   const hasType = text.match(/type \d/gi)
   const hasBoard = text.match(/board "(.*?)"/gi)
   const isHelp = text.match(/--help|--h/gi)
   if(isHelp){
-    return replyWrongPattern(action, 'missing_require')
+    return replyWrongPattern(action, 'help')
   }
   if (hasName) {
     const name = hasName[0].replace(/"|name| /g, '')
